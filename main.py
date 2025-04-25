5e4e701374251bb3215fbc3cdeb80f9dc31aa5dd
Max H <max.mhodin@gmail.com>
2025-04-25 15:53:05 -0400

    addded gaussian graphs, not working currently

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy.stats import norm
+
+def fit_gaussian(dataset):
+    mean = np.mean(dataset) #uses arithmetic mean formula
+    var = np.var(dataset) #uses sum of difference of mean squares formula
+    x = np.linspace(np.min(dataset), np.max(dataset), 1000)
+    return norm.pdf(x, loc=mean, scale = np.sqrt(var))
 
 df = pd.read_excel("mlb-odds-2021.xlsx")
 #Note that, for even n, the nth + 1 team was their opponent
@@ -24,18 +31,18 @@ for i in range(int(len(data)/2)):
     #case that team 1 wins
     if data.iloc[2*i,1] > data.iloc[2*i + 1,1]:
         if t1odd > 0: #t1 is the underdog
-            profit_und[last_index] += t1odd
-            profit_fav[last_index] -= 100
+            profit_und[last_index] += t1odd /100
+            profit_fav[last_index] -= 1
         else: #t1 was the favorite
-            profit_fav[last_index] -= 10000 / t1odd #subtract due to negative sign
-            profit_und[last_index] -= 100
+            profit_fav[last_index] -= 100 / t1odd #subtract due to negative sign
+            profit_und[last_index] -= 1
     else:
         if t2odd > 0: #t2 is the underdog
-            profit_und[last_index] += t2odd
-            profit_fav[last_index] -= 100
+            profit_und[last_index] += t2odd /100
+            profit_fav[last_index] -= 1
         else: #t2 was the favorite
-            profit_fav[last_index] -= 10000 / t2odd #subtract due to negative sign
-            profit_und[last_index] -= 100
+            profit_fav[last_index] -= 100 / t2odd #subtract due to negative sign
+            profit_und[last_index] -= 1
     if data.iloc[2*i+1,0] != prev_date:
         prev_date = data.iloc[2*i+1,0]
         profit_fav[last_index] /= day_games
@@ -45,9 +52,17 @@ for i in range(int(len(data)/2)):
         day_games = 0
 
 
-plt.figure()
-plt.hist(profit_fav, color="red",bins=40,label="Betting on the favorite",alpha=.80)
-plt.hist(profit_und, color="blue",bins=40,label="Betting on the underdog",alpha=.80)
-plt.title("Frequency of profit in 2021 season")
+plt.figure()    
+fig, ax1 = plt.subplots()
+ax1.hist(profit_fav, color="red",bins=40,label="Betting on the favorite",alpha=.80)
+ax1.hist(profit_und, color="blue",bins=40,label="Betting on the underdog",alpha=.80)
+ax2 = ax1.twinx()
+x = np.linspace(-1, 2, 1000)
+ax2.plot(x, fit_gaussian(profit_fav), label="Favorite Fit")
+ax2.plot(x, fit_gaussian(profit_und), label="Underdog Fit")
+ax1.set_title("Frequency of profit in 2021 season")
+ax1.set_xlabel("Expected profit per game bet on, daily")
+ax1.set_xlim(-1, 2)
+ax2.set_xlim(-1, 2)
 plt.legend()
 plt.savefig("ee24finalprojectplot1")
